4750e300b92533794fafceeaa8ebb36ae1b0680b
Adrian <u201718169@upc.edu.pe>
2022-06-03 22:03:35 -0500

    Initial commit

---
+++ src/test/java/com/goback/restore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goback.restore.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
